0b39437b84040a5513809c207d6ec3cca1d6ff97
Jessica Rosenauer <jessica.l.rosenauer@gmail.com>
2017-01-30 08:50:41 -0600

    JEROINDWRK-4

---
+++ DogGenetics/src/main/java/com/mycompany/doggenetics/DogGenetics.java
@@ -0,0 +1,35 @@
+package com.mycompany.doggenetics;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author apprentice
+ */
+public class DogGenetics {
+    
+    public static void main(String [] args) {
+        
+        
+        int percentage = 100;
+        int stBernard = 
+        int chi = 
+        int goldenRet = 
+        int dach = 
+        int sibHusky = 
+        
+        
+        Random randomizer = percentage Random();
+        Scanner inputReader = percentage Scanner(System.in);
+
+System.out.println("What is your dog's name?");
+System.out.println("Well then, I have this highly reliable report on your dog's prestigious background right here:"
+System.out.println(
+
