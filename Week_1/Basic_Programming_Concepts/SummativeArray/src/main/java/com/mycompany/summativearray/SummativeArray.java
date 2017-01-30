5ca100e3829004fb3958da2b2e93816a4b417ee6
Jessica Rosenauer <jessica.l.rosenauer@gmail.com>
2017-01-30 16:34:50 -0600

    JEROINDWRK-3

--- Week_1/Basic_Programming_Concepts/SummativeArray/src/main/java/com/mycompany/summativearray/SummativeArray.java
+++ Week_1/Basic_Programming_Concepts/SummativeArray/src/main/java/com/mycompany/summativearray/SummativeArray.java
@@ -5,31 +5,41 @@ package com.mycompany.summativearray;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author apprentice
  */
 public class SummativeArray {
-    public static void main(String [] args) {
-        
+
+    public static void main(String[] args) {
+
         int array1[] = {1, 90, -33, -55, 67, -16, 28, -55, 15};
         int array2[] = {999, -60, -77, 14, 160, 301};
         int array3[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99};
-        
-        int sum1 = 0;
-        for (arrayPosition = 0; arrayPosition < array1.length)
+
+        /*int sum1 = 0;
+        for (arrayPosition = 0; arrayPosition1 < array1.length) {
             sum1 += 1;
-        int sum2 = 0;
-        for (var arrayPosition2 = 0; arrayPosition < array2.length)
-            sum2 += 1;
-        int sum3 = 0;
-        for (var arrayPosition3 = 0; arrayPosition < array3.length)
-            sum3 += 1;
-            
-        System.out.println("#1 Array Sum: " + sum1);
-        System.out.println("#2 Array Sum: " + sum2);
-        System.out.println("#3 Array Sum: " + sum3);
-        
+            int sum2 = 0;
+            for (arrayPosition2 = 0; arrayPosition2 < array2.length) {
+                sum2 += 1;
+                int sum2 = 0;
+                for (array3.length == 0; array3.length == 5; array3[] += sum3) {
+                    
+         */
+        System.out.println("#1 Array Sum: " + SumArray(array1));
+        System.out.println("#2 Array Sum: " + SumArray(array2));
+        System.out.println("#3 Array Sum: " + SumArray(array3));
+
     }
+
+    private static int SumArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+
+       }
 }
+
